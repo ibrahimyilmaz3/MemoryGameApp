fdb57e4de7d2c8f936d7a29250ed761a37851d4e
arcticdeveloper1 <ibrahimyilmaz99999@gmail.com>
2022-07-26 16:08:05 +0300

    Memory Game App (StartActivityForResult was deprecated, not completed)

---
+++ app/src/main/java/com/example/mymemory/utils/CreateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mymemory.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mymemory.R
+
+class CreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+    }
+}
